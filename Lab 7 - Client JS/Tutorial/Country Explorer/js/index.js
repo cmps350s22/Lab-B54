0d005b7efee10e444b5911233ab60927f8ee3a6f
Abdulahi Hassen <abdala.baaji@gmail.com>
2022-02-23 09:22:21 +0300

    updated

--- Lab 7 - Client JS/Tutorial/Country Explorer/js/index.js
+++ Lab 7 - Client JS/Tutorial/Country Explorer/js/index.js
@@ -1,9 +1,13 @@
+//STEP 1: GET THE REFERENCE TO THE HTML ELEMENTS
 const regionSearchBar = document
     .querySelector('#region')
-const countryDropdown = document.querySelector('#country')
-
+const countryDropdown = document.querySelector('#country') //refernece
+const factsArea = document.querySelector('#facts-area')
+//STEP 2: BASE URL
 const regionBaseUrl = 'https://restcountries.com/v3.1/region'
+const countryBaseUrl = 'https://restcountries.com/v3.1/name'
 
+//STEP 3 : CREATE THE CALL BACK FUNCTIONS
 async function getCountries() {
     const url = `${regionBaseUrl}/${regionSearchBar.value}` //create the url
     const data = await fetch(url)  //download the data
@@ -13,12 +17,43 @@ async function getCountries() {
         .join(' ')
 
     countryDropdown.innerHTML = options
+
+
 }
 
 function countryToOption(country) {
     return `<option value="${country.name.common}">${country.name.common}</option>`
 }
 
+async function handleCountryChange() {
+    const url = `${countryBaseUrl}/${countryDropdown.value}` //create the url
+    const data = await fetch(url)  //download the data
+    const countries = await data.json()  //
+    const countryToFactsHTML = countryToFacts(countries[0])
+    factsArea.innerHTML = countryToFactsHTML
+}
+
+function countryToFacts(country){
+    return `
+        <h1>Facts about ${country.name.common}</h1>
+        <img src="${country.flags.png}" alt="">
+        <table>
+            <tr>
+                <td>Offical Country Name</td>
+                <td>${country.name.official}</td>
+            </tr>
+            <tr>
+                <td>Population</td>
+                <td>${country.population}</td>
+            </tr>
+              <tr>
+                <td>Currencies</td>
+                <td>${Object.keys(country.currencies).map(key => country.currencies[key].name)}</td>
+            </tr>
+        </table>
+        
+    `
+}
 
 
 
