3677222736d76abc8154969266494e7d99708657
Abdulahi Hassen <abdala.baaji@gmail.com>
2022-03-01 18:46:52 +0300

    Update index.js

--- Lab 8 - Client Data Management/Tutorial/CensusApp/js/index.js
+++ Lab 8 - Client Data Management/Tutorial/CensusApp/js/index.js
@@ -1,16 +1,19 @@
 import {CensusRepo} from "./repository/census-repo.js";
+
 const repo = new CensusRepo()
 window.onload = async () => {
     await showCensusData();
 }
 
+const countriesTable = document.querySelector('#countries')
 const form = document.querySelector('#form')
 form.addEventListener('submit', addCensus)
 
-function addCensus(e) {
+async function addCensus(e) {
     e.preventDefault();
     const census = formToObject(e.target)
-    repo.addCensus(census)
+    await repo.addCensus(census)
+    await showCensusData();
 }
 
 function formToObject(form) {
@@ -24,8 +27,33 @@ function formToObject(form) {
 }
 
 async function showCensusData() {
-    //show the census
+    const censuses = await repo.getCensuses();
+    const htmlRows = censuses.map(census => censusToHTMLRow(census)).join(' ')
+    countriesTable.innerHTML = `
+        <tr>
+            <th>Country</th>
+            <th>Population</th>
+            <th>Action</th>
+        </tr>
+        ${htmlRows}
+    `
 }
+
+function censusToHTMLRow(census) {
+    return `
+        <tr>
+            <td>${census.country}</td>
+            <td>${census.population}</td>
+            <td>
+                <i class="fa fa-edit">Edit</i>
+                <i class="fa fa-tash">Delete</i>
+            </td>
+        </tr>
+    
+    `;
+}
+
+
 
 
 
