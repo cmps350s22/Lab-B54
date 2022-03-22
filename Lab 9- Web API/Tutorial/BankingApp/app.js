b5f46db35a6df0f067d81c031bf280fbb9dd8588
Abdulahi Hassen <abdala.baaji@gmail.com>
2022-03-22 17:27:08 +0300

    updated

---
+++ Lab 9- Web API/Tutorial/BankingApp/app.js
@@ -0,0 +1,16 @@
+/*
+    1. initialize the package
+        npm init -y
+
+    2. add the type module to your package
+        "type" : "module"
+
+    3. install express
+       npm i express
+
+    4. Install nodemon to automatically
+       restart our sever everytime we see changes
+
+       npm i -g nodemon [window]
+       sudo npm i -g nodemon [Mac]
+ */
