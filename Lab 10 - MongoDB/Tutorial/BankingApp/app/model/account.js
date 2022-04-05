0c90f72ed325a832bb49144d2775a5fa90659094
Abdulahi Hassen <abdala.baaji@gmail.com>
2022-04-05 21:26:01 +0300

    updated

---
+++ Lab 10 - MongoDB/Tutorial/BankingApp/app/model/account.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const options = {
+    toJSON: {
+        virtuals: true
+    }
+}
+const accountSchema = new Schema({
+    acctType: {
+        type: String,
+        required: [true, 'acctType is a required field'],
+        enum: ['Current', 'Saving']
+    },
+    balance: {
+        type: Number,
+        required: [true, 'balance is a required field'],
+        min: [0, 'balance can not be negative']
+    }
+}, options)
+
+accountSchema.virtual('minBalance').get(function () {
+    if(this.acctType === 'Saving')
+        return 1000
+})
+accountSchema.virtual('monthlyFee').get(function () {
+    if(this.acctType === 'Current')
+        return 15
+})
+
+export default mongoose.model('Account', accountSchema)
+
+
+
+
+
+
+
