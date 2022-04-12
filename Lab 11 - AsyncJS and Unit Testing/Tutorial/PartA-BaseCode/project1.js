b5110e520d01c12257f9198e43be702e08575c88
Abdulahi Hassen <abdala.baaji@gmail.com>
2022-04-12 20:36:54 +0300

    updated p2

--- Lab 11 - AsyncJS and Unit Testing/Tutorial/PartA-BaseCode/project1.js
+++ Lab 11 - AsyncJS and Unit Testing/Tutorial/PartA-BaseCode/project1.js
@@ -1,10 +1,34 @@
-const fs=require('fs');
+const fs = require('fs');
 //Synchronous code. Change it to async using callback.
-let data = fs.readFileSync('data/student.json');
-console.log(JSON.parse(data));
+// let data = fs.readFileSync('data/student.json');
+// console.log(JSON.parse(data));
+// console.log('This will execute now')
 
 /*
 1.Convert this code to asynchronous form using a separate callback function (callBack).
 2.Change the callback function to an anonymous one.
 2.Take care of error handling in the callback function
 */
+
+
+// function readFile(filepath, cb){
+//     //read the file
+//     const isError = false
+//     const data = [1,2,3,4,5,6,7]
+//     const err = "An error happened"
+//     if(!isError){
+//         cb(null, data)
+//     }else{
+//         cb(err, null)
+//     }
+// }
+
+
+fs.readFile('data/student.json', (err, data) => {
+    if (!err)
+        console.log(JSON.parse(data))
+    else
+        console.log(err)
+});
+
+console.log('This will execute now')
--- Lab 11 - AsyncJS and Unit Testing/Tutorial/PartA-BaseCode/project2.js
+++ Lab 11 - AsyncJS and Unit Testing/Tutorial/PartA-BaseCode/project2.js
@@ -9,4 +9,23 @@ function getCourses(cb)
 4)	Instructor names are set as a new property to the course object in the setInstrctorNames function.
 
  */
+import fs from 'fs';
+function setInstructorNames(courses, cb) {
 
+}
+
+function getCourses(cb) {
+    fs.readFile('data/course.json', (err, data) => {
+        if (!err) {
+            cb(null, JSON.parse(data))
+        } else
+            cb(err, null)
+    });
+}
+
+getCourses((err, data)=>{
+    if(!err)
+        console.log(data)
+    else
+        console.log(err)
+})
