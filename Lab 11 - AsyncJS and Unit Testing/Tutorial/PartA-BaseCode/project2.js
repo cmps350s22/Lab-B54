af01dae51038c2da56e32a605f2905b8b8a537a5
Abdulahi Hassen <abdala.baaji@gmail.com>
2022-04-12 21:22:15 +0300

    Update project2.js

--- Lab 11 - AsyncJS and Unit Testing/Tutorial/PartA-BaseCode/project2.js
+++ Lab 11 - AsyncJS and Unit Testing/Tutorial/PartA-BaseCode/project2.js
@@ -10,21 +10,54 @@ function getCourses(cb)
 
  */
 import fs from 'fs';
-function setInstructorNames(courses, cb) {
 
-}
-
-function getCourses(cb) {
-    fs.readFile('data/course.json', (err, data) => {
+function setCourseStudents(courses, cb) {
+    fs.readFile('data/student.json', (err, data) => {
         if (!err) {
-            cb(null, JSON.parse(data))
+            const students = JSON.parse(data)
+            for (const course of courses) {
+                const studentNames = students
+                    .filter(student => student.courseIds.includes(course.crn))
+                    .map(s => `${s.firstname} ${s.lastname}`)
+                course.students = studentNames
+                course.noOfStudent = studentNames.length
+            }
+            cb(null, courses)
+
         } else
             cb(err, null)
     });
 }
 
-getCourses((err, data)=>{
-    if(!err)
+function setInstructorNames(courses, cb) {
+    fs.readFile('data/staff.json', (err, data) => {
+        if (!err) {
+            const staff = JSON.parse(data)
+            for (const course of courses) {
+                const {firstname, lastname} = staff.find(s => s.staffNo == course.instructorId)
+                course.instructor = `${firstname} ${lastname}`
+            }
+            setCourseStudents(courses, cb)
+
+        } else
+            cb(err, null)
+    });
+}
+
+function getCourses(cb) {
+    fs.readFile('data/course.json', (err, data) => {
+        if (!err) {
+            const courses = JSON.parse(data)
+            setInstructorNames(courses, cb)
+        } else
+            cb(err, null)
+    });
+}
+
+//calling the function
+
+getCourses((err, data) => {
+    if (!err)
         console.log(data)
     else
         console.log(err)
